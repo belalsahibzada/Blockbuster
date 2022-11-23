c16859c3d4585f1a7cd735f85319b5f09936ec6c
belalsahibzada <93454804+belalsahibzada@users.noreply.github.com>
2022-11-22 22:14:34 -0500

    Cosmetic changes

--- server/controller/index.js
+++ server/controller/index.js
@@ -3,5 +3,5 @@ let router = express.Router();
 
 //controller for index page -> used to organize code
 module.exports.displayHomePage = (req, res, next) => {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Blockbuster' });
   }
