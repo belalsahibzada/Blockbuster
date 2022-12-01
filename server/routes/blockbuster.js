c8d58375c1ac4a87add8d6e03d6fa63221244a54
landonSmyth <landon.smyth@ontariotechu.net>
2022-12-01 13:28:40 -0500

    Rough register/login pages, auth function
    Co-authored-by: marcocagg <marcocagg@users.noreply.github.com>
    Co-authored-by: belalsahibzada <belalsahibzada@users.noreply.github.com>
    Co-authored-by: mutasim1475 <mutasim1475@users.noreply.github.com>

--- server/routes/blockbuster.js
+++ server/routes/blockbuster.js
@@ -2,30 +2,39 @@ let express = require('express')
 let router = express.Router();
 let mongoose = require('mongoose');
 
-// need to connect with book model
+// need to connect with blockbuster model
 let blockbuster = require('../models/blockbuster')
 let blockbusterController = require('../controller/blockbuster')
 
+function requireAuth(req,res,next)
+{
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 /*CRUD Operation */
 // Read operation 
-// Get route for the book list 
+// Get route for the movie list 
 router.get('/',blockbusterController.displayMovieList)
 
 /*Perform add operation  */
 /*Get route for displaying the ADD-Page -- Create Operation */
-router.get('/add',blockbusterController.displayAddPage)
+router.get('/add', requireAuth, blockbusterController.displayAddPage)
 
 /*Post route for processing the ADD-Page -- Create Operation */
-router.post('/add',blockbusterController.processAddPage)
+router.post('/add', requireAuth, blockbusterController.processAddPage)
 
 /*Perform edit operation  */
 /*Get route for displaying the Edit Operation -- Update Operation */
-router.get('/edit/:id',blockbusterController.displayEditPage)
+router.get('/edit/:id', requireAuth, blockbusterController.displayEditPage)
 /*Post route for displaying the Edit Operation -- Update Operation */
-router.post('/edit/:id',blockbusterController.processEditPage)
+router.post('/edit/:id', requireAuth, blockbusterController.processEditPage)
 
 /*Perform delete operation  */
 /*Get to perform Delete Operation -- Deletion */
-router.get('/delete/:id',blockbusterController.performDeletePage)
+router.get('/delete/:id', requireAuth, blockbusterController.performDeletePage)
 
 module.exports = router;
