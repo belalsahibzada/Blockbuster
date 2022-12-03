ca59d73f42f3cc397a384bbacf278ec12fe45516
landonSmyth <landon.smyth@ontariotechu.net>
2022-12-03 00:03:51 -0500

    Revised Authentication Functionality

--- server/routes/blockbuster.js
+++ server/routes/blockbuster.js
@@ -18,23 +18,23 @@ function requireAuth(req,res,next)
 /*CRUD Operation */
 // Read operation 
 // Get route for the movie list 
-router.get('/',blockbusterController.displayMovieList)
+router.get('/', blockbusterController.displayMovieList)
 
 /*Perform add operation  */
 /*Get route for displaying the ADD-Page -- Create Operation */
-router.get('/add',  blockbusterController.displayAddPage)
+router.get('/add', requireAuth, blockbusterController.displayAddPage)
 
 /*Post route for processing the ADD-Page -- Create Operation */
-router.post('/add', blockbusterController.processAddPage)
+router.post('/add', requireAuth, blockbusterController.processAddPage)
 
 /*Perform edit operation  */
 /*Get route for displaying the Edit Operation -- Update Operation */
-router.get('/edit/:id',  blockbusterController.displayEditPage)
+router.get('/edit/:id', requireAuth, blockbusterController.displayEditPage)
 /*Post route for displaying the Edit Operation -- Update Operation */
-router.post('/edit/:id',  blockbusterController.processEditPage)
+router.post('/edit/:id', requireAuth, blockbusterController.processEditPage)
 
 /*Perform delete operation  */
 /*Get to perform Delete Operation -- Deletion */
-router.get('/delete/:id', blockbusterController.performDeletePage)
+router.get('/delete/:id', requireAuth, blockbusterController.performDeletePage)
 
 module.exports = router;
